67944205ced2ba48b24d8bf07330c7f198d61d25
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-11-09 16:42:22 +0600

    added vercel.json file

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-      origin: 'http://localhost:5173',
+      origin: [
+            'http://localhost:5173',
+            // 'https://dev-connect-bd.web.app',
+            // 'https://dev-connect-bd.firebaseapp.com'
+      ],
       credentials: true
 }));
 app.use(express.json());
