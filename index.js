d8bac9501c89ccb49bbd0da7c74846b537826c06
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-11-09 11:54:44 +0600

    added jwt.io to create token and send it to brouser cookie

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express();
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -11,6 +13,7 @@ app.use(cors({
       credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d8abmis.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,11 +28,59 @@ const client = new MongoClient(uri, {
 });
 
 const allJobCollection = client.db("devConnectBdDB").collection('allJobs');
+const appliedJobCollection = client.db("devConnectBdDB").collection('appliedJobs');
+
+// middleware 
+
+// verify token
+// verifyToken
+const verifyToken = (req, res, next) => {
+      const token = req.cookies.token;
+      if (!token) {
+            res.status(401).send({ message: 'Unauthorized Access' })
+      }
+      jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+            if (err) {
+                  res.status(401).send({ message: 'Unauthorized Access' })
+            }
+            req.user = decoded
+            console.log('decoded', decoded);
+            next()
+      })
+}
+
+
 
 async function run() {
       try {
             // Connect the client to the server	(optional starting in v4.7)
             await client.connect();
+
+            // jwt
+            app.post('/api/v1/auth/jwt', async (req, res) => {
+                  const user = req.body;
+                  const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' })
+                  res
+                        .cookie('token', token, {
+                              httpOnly: true,
+                              secure: true,
+                              sameSite: 'none',
+                        })
+                        .send({ message: true })
+
+            })
+
+            // clear token
+            app.post('/api/v1/auth/logout', async (req, res) => {
+                  const user = req.body;
+                  res
+                        .clearCookie('token', {
+                              maxAge: 0,
+                              secure: true,
+                              sameSite: 'none',
+                        },)
+                        .send({ success: true })
+            })
 
             // user api
             app.post('/api/v1/allJobs', async (req, res) => {
@@ -44,8 +95,13 @@ async function run() {
                   res.send(result)
             })
 
-            app.get('/api/v1/jobs', async (req, res) => {
-                  console.log(req.query.userEmail);
+            app.get('/api/v1/jobs', verifyToken, async (req, res) => {
+
+                  // // verify token owner
+                  if (req.query?.userEmail !== req.user?.email) {
+                        return res.status(403).send({ message: 'Forbidden' })
+                  }
+
                   let query = {};
                   if (req.query.userEmail) {
                         query = { userEmail: req.query.userEmail }
@@ -70,27 +126,33 @@ async function run() {
                   res.send(result)
             })
 
-            //  // update product
-            //  app.put('/products/:id', async (req, res) => {
-            //       const id = req.params.id;
-            //       const product = req.body;
-            //       const filter = { _id: new ObjectId(id) }
-            //       const options = { upsert: true };
-            //       const updatedProduct = {
-            //             $set: {
-            //                   image: product.image,
-            //                   name: product.name,
-            //                   brand: product.brand,
-            //                   type: product.type,
-            //                   price: product.price,
-            //                   rating: product.rating,
-            //             },
-            //       };
-            //       const result = await productsCollection.updateOne(filter, updatedProduct, options);
-            //       res.send(result);
-            // })
+            // get job by id || single job || increment applicants
+            app.get('/api/v1/allJobs/applicants/number/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) }
+                  const result = await allJobCollection.findOne(query);
+                  res.send(result)
+            })
 
-            // update data || put method
+            // put/update job by id || single job || increment applicants
+            app.put('/api/v1/allJobs/applicants/number/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) };
+                  const job = await allJobCollection.findOne(query);
+                  if (job) {
+                        const updatedApplicantsNumber = job.applicantsNumber + 1;
+
+                        const updateDoc = {
+                              $set: {
+                                    applicantsNumber: updatedApplicantsNumber
+                              }
+                        };
+                  }
+                  const result = await allJobCollection.updateOne(query, updateDoc);
+                  res.send(result);
+            })
+
+            // update job || put method
             app.put('/api/v1/allJobs/singleJobs/:id', async (req, res) => {
                   const id = req.params.id;
                   const job = req.body;
@@ -104,6 +166,46 @@ async function run() {
                   const result = await allJobCollection.updateOne(query, updateDoc, options)
                   res.send(result)
             })
+
+            // delete job
+            app.delete('/api/v1/allJobs/singleJobs/:id', async (req, res) => {
+                  const id = req.params.id;
+                  const query = { _id: new ObjectId(id) }
+                  const result = await allJobCollection.deleteOne(query);
+                  res.send(result);
+            })
+
+
+            // user || applied jobs
+
+            app.get('/api/v1/user/appliedJobs', verifyToken, async (req, res) => {
+
+                  // verify token owner
+                  if (req.query?.loggedEmail !== req.user?.email) {
+                        return res.status(403).send({ message: 'Forbidden' })
+                  }
+
+                  let query = {};
+                  if (req.query.loggedEmail) {
+                        query = { loggedEmail: req.query.loggedEmail };
+                  }
+                  const result = await appliedJobCollection.find(query).toArray();
+                  res.send(result);
+            });
+
+            // applied jobs
+            app.post('/api/v1/user/appliedJobs', async (req, res) => {
+                  const job = req.body;
+                  const isExist = await appliedJobCollection.findOne({ jobId: job.jobId, loggedUser: job.loggedUser })
+
+                  if (isExist) {
+                        res.send({ message: 'Already Added' })
+                        return;
+                  }
+                  const result = await appliedJobCollection.insertOne(job);
+                  res.send(result)
+            })
+
 
             // Send a ping to confirm a successful connection
             await client.db("admin").command({ ping: 1 });
